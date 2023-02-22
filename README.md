b0e5642c6a4288b9f972f81e17f2df0d9fe27b27
sreqtor <99046539+sreqtor@users.noreply.github.com>
2023-02-22 17:52:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tiendaEjemploAngular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zmyo4a)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share() {
+    window.alert('¡El producto ha sido compartido!');
+  }
+
+  onNotify() {
+    window.alert('Recibirás una notificación cuando el producto sea vendido');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
